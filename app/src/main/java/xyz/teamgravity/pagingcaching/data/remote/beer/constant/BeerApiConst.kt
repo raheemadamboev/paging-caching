425b82370d0b25f063ba62937f2ac26ccc39668c
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-06 23:17:46 +0500

    Added BeerApiConst;

---
+++ app/src/main/java/xyz/teamgravity/pagingcaching/data/remote/beer/constant/BeerApiConst.kt
@@ -0,0 +1,11 @@
+package xyz.teamgravity.pagingcaching.data.remote.beer.constant
+
+object BeerApiConst {
+
+    const val BASE_URL = "https://api.punkapi.com/v2/"
+
+    object Query {
+        const val PAGE = "page"
+        const val PER_PAGE = "per_page"
+    }
+}
