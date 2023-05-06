eb57266eb8f376b1dacc07b28b0c031a3da311cd
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-06 23:19:07 +0500

    Added BeerDatabaseConst;

---
+++ app/src/main/java/xyz/teamgravity/pagingcaching/data/local/beer/constant/BeerDatabaseConst.kt
@@ -0,0 +1,22 @@
+package xyz.teamgravity.pagingcaching.data.local.beer.constant
+
+object BeerDatabaseConst {
+
+    /**
+     * Database name.
+     */
+    const val NAME = "database_beer"
+
+    /**
+     * Database version.
+     */
+    const val VERSION = 1
+
+    /**
+     * Table Beer.
+     * Dto -> [fully qualified dto name]
+     * Entity -> [fully qualified entity name]
+     * Model -> [fully qualified model name]
+     */
+    const val TABLE_BEER = "table_beer"
+}
