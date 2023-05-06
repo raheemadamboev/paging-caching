6c79264eb02df7e04e1747f9db113ba45a6890a4
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-06 22:37:25 +0500

    Setup hilt;

--- app/src/main/java/xyz/teamgravity/pagingcaching/presentation/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/pagingcaching/presentation/activity/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import xyz.teamgravity.pagingcaching.presentation.theme.PagingCachingTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
