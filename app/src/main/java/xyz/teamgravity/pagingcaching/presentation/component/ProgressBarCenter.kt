84f7e346c11a53506d616b5fae132475d57c97fd
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-07 17:57:24 +0500

    Added ProgressBarCenter;

---
+++ app/src/main/java/xyz/teamgravity/pagingcaching/presentation/component/ProgressBarCenter.kt
@@ -0,0 +1,18 @@
+package xyz.teamgravity.pagingcaching.presentation.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProgressBarCenter() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        CircularProgressIndicator()
+    }
+}
