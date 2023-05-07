256e2ae890e2c66567fd6a0034fe7f48f8218f2c
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-07 17:07:53 +0500

    Bound dependencies;

--- app/src/main/java/xyz/teamgravity/pagingcaching/injection/bind/ApplicationModule.kt
+++ app/src/main/java/xyz/teamgravity/pagingcaching/injection/bind/ApplicationModule.kt
@@ -1,11 +1,15 @@
 package xyz.teamgravity.pagingcaching.injection.bind
 
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Converter
 import retrofit2.converter.moshi.MoshiConverterFactory
+import xyz.teamgravity.pagingcaching.data.repository.MainRepositoryImp
+import xyz.teamgravity.pagingcaching.domain.repository.MainRepository
 import javax.inject.Singleton
 
 @Module
@@ -14,5 +18,13 @@ abstract class ApplicationModule {
 
     @Binds
     @Singleton
+    abstract fun bindJsonAdapterFactory(kotlinJsonAdapterFactory: KotlinJsonAdapterFactory): JsonAdapter.Factory
+
+    @Binds
+    @Singleton
     abstract fun bindConverterFactory(moshiConverterFactory: MoshiConverterFactory): Converter.Factory
+
+    @Binds
+    @Singleton
+    abstract fun bindMainRepository(mainRepositoryImp: MainRepositoryImp): MainRepository
 }
