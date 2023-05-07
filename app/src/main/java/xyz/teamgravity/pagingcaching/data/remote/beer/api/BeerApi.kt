c287f72ff79f731a9fc2c1cf1ec7bc093b41bb21
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-07 17:15:35 +0500

    Added BeerApi;

--- app/src/main/java/xyz/teamgravity/pagingcaching/data/remote/beer/api/BeerApi.kt
+++ app/src/main/java/xyz/teamgravity/pagingcaching/data/remote/beer/api/BeerApi.kt
@@ -2,14 +2,14 @@ package xyz.teamgravity.pagingcaching.data.remote.beer.api
 
 import retrofit2.http.GET
 import retrofit2.http.Query
-import xyz.teamgravity.pagingcaching.data.remote.beer.constant.BeerApiConst
+import xyz.teamgravity.pagingcaching.data.remote.beer.constant.BeerApiConst.GetBeers
 import xyz.teamgravity.pagingcaching.data.remote.beer.dto.BeerDto
 
 interface BeerApi {
 
-    @GET
+    @GET(GetBeers.ENDPOINT)
     suspend fun getBeers(
-        @Query(BeerApiConst.Query.PAGE) page: Int,
-        @Query(BeerApiConst.Query.PER_PAGE) perPage: Int,
+        @Query(GetBeers.Query.PAGE) page: Int,
+        @Query(GetBeers.Query.PER_PAGE) perPage: Int,
     ): List<BeerDto>
 }
